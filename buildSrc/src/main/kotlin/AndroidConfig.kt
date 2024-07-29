e350460df2745c2523b9b861f42c2e1518ddbca6
hyojin <jin33032@gmail.com>
2024-07-29 22:45:08 +0900

    fix : build tool version update

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -8,7 +8,7 @@ object AndroidConfig {
     const val VERSION_CODE = 11
     const val VERSION_NAME = "2.0.0"
 
-    const val BUILD_TOOLS_VERSION = "34.0.0"
+    const val BUILD_TOOLS_VERSION = "35.0.0"
     const val APPLICATION_ID = "com.ddd4.synesthesia.beer"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
     val JAVA_VERSION = JavaVersion.VERSION_17
